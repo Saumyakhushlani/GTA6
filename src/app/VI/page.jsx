441e4c8a2f5cab6fbe651b64d29549d6a9d94769
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-06-20 02:06:05 +0530

    Images

--- src/app/VI/page.jsx
+++ src/app/VI/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '@/Components/Navbar'
+import Landing from '@/Components/Landing'
+
 
 const page = () => {
   return (
     <div className='bg-[#111111] min-h-screen'>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Landing/>
     </div>
   )
 }
