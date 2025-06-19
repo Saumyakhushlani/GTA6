c8ea1774518d277bdfa85d8e4d2b303cbd9bb578
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-06-19 22:28:08 +0530

    add download in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useState } from 'react'
+import { MoveRight } from 'lucide-react'
 
 const Navbar = () => {
     const [isnavbarOpen, setNavbarOpen] = useState(false)
     const [selectedtab, setSelectedtab] = useState("People")
     const [image, setImage] = useState("/logo.jpeg")
     const [imageKey, setImageKey] = useState(0)
+    const [isHovered, setIsHovered] = useState("")
 
     const tabs = ["People", "Places", "Trailers", "Downloads"]
 
@@ -31,6 +33,12 @@ const Navbar = () => {
         { name: "MOUNT KALAGA", img: "/mount.webp" }
     ];
 
+    const downloads = [
+      { name: "VIDEOS", img: "/navbar_down_videos.webp" },
+      { name: "SCREENSHOTS", img: "/navbar_down_screenshot.webp" },
+      { name: "ARTWORK & WALLPAPERS", img: "/navbar_down_artwork.webp" },
+    ];
+
     const handleImageChange = (newImage) => {
         setImage(newImage)
         setImageKey(prev => prev + 1)
@@ -42,142 +50,183 @@ const Navbar = () => {
     }
 
     return (
-        <div className='relative'>
+      <div className="relative">
+        <div className="flex flex-row justify-between items-center text-2xl p-3 relative">
+          <div className="font-bold text-white text-4xl">VI</div>
 
-            <div className='flex flex-row justify-between items-center text-2xl p-3 relative'>
-                <div className='font-bold text-white text-4xl'>VI</div>
-
-                <button
-                    onClick={() => setNavbarOpen(!isnavbarOpen)}
-                    className='fixed z-50 right-4 top-4 w-8 h-8 flex flex-col justify-center items-center gap-1'
-                >
-                    <motion.span
-                        className="w-6 h-1 bg-white block"
-                        animate={{
-                            rotate: isnavbarOpen ? 45 : 0,
-                            y: isnavbarOpen ? 4 : 0,
-                        }}
-                        transition={{ duration: 0.3, ease: "easeInOut" }}
-                    />
-                    <motion.span
-                        className="w-6 h-1 bg-white block"
-                        animate={{
-                            rotate: isnavbarOpen ? -45 : 0,
-                            y: isnavbarOpen ? -4 : 0,
-                        }}
-                        transition={{ duration: 0.3, ease: "easeInOut" }}
-                    />
-                </button>
-            </div>
-
-            <AnimatePresence>
-                {isnavbarOpen && (
-                    <motion.div
-                        initial={{ x: "100%" }}
-                        animate={{ x: 0 }}
-                        exit={{ x: "100%" }}
-                        transition={{ duration: 0.5 }}
-                        className='fixed top-0 right-0 h-screen bg-gradient-to-r from-[#16151f] to-[#181622] md:w-[45vw] w-[90vw] z-40 shadow-2xl md:p-10 p-5 md:pt-0 pt-15'
-                    >
-                        <div className='flex rounded-full my-6 md:gap-3 justify-start mb-10'>
-                            {tabs.map((tab) => (
-                                <button
-                                    key={tab}
-                                    onClick={() => setSelectedtab(tab)}
-                                    className={`md:px-5 px-3 py-3 md:text-md text-sm font-semibold rounded-full transition-all duration-300 ${selectedtab === tab
-                                        ? 'bg-white text-gray-900'
-                                        : 'text-gray-300 hover:text-white'
-                                        }`}
-                                >
-                                    {tab}
-                                </button>
-                            ))}
-                        </div>
-
-                        <div className='text-white'>
-                            {selectedtab === "People" && (
-                                <div className='flex flex-col md:justify-center h-[60vh] pl-2'>
-                                    {peoples.map((e) => (
-                                        <div 
-                                            key={e.name} 
-                                            className='hover:text-[#fff9cb] text-white md:text-6xl text-4xl mt-0.6 font-sans font-semibold md:tracking-tighter' 
-                                            onMouseEnter={() => handleImageChange(e.img)} 
-                                            onMouseLeave={() => handleImageReset()}
-                                        >
-                                            {e.name}
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-
-                            {selectedtab === "Places" && (
-                                <div className='flex flex-col md:justify-center h-[60vh] pl-2'>
-                                    {places.map((e) => (
-                                        <div 
-                                            key={e.name} 
-                                            className='hover:text-[#fff9cb] text-white md:text-6xl text-4xl mt-0.6 font-sans font-semibold md:tracking-tight' 
-                                            onMouseEnter={() => handleImageChange(e.img)} 
-                                            onMouseLeave={() => handleImageReset()}
-                                        >
-                                            {e.name}
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-
-                            {selectedtab === "Trailers" && (
-                                <div>
-                                    <p>Trailers content goes here</p>
-                                </div>
-                            )}
-
-                            {selectedtab === "Downloads" && (
-                                <div>
-                                    <p>Downloads content goes here</p>
-                                </div>
-                            )}
-                        </div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-
-            <AnimatePresence mode="wait">
-                {isnavbarOpen && (
-                    <motion.img
-                        key={imageKey} 
-                        src={image}
-                        className='w-[75vw] fixed left-0 z-10 top-0 object-cover h-full'
-                        initial={{ 
-                            x: 0, 
-                             
-                            opacity: 0,
-                            filter: "blur(10px)"
-                        }}
-                        animate={{ 
-                            x: "-10vw", 
-                          
-                            opacity: 1,
-                            filter: "blur(0px)"
-                        }}
-                        exit={{ 
-                            
-                            opacity: 0,
-                            filter: "blur(5px)",
-                            transition: { duration: 0.3 }
-                        }}
-                        transition={{ 
-                            duration: 2,
-                            ease: "easeOut",
-                            scale: { duration: 0.6 },
-                            filter: { duration: 0.4 },
-                            opacity:{duration:0.4}
-                        }}
-                    />
-                )}
-            </AnimatePresence>
-
+          <button
+            onClick={() => setNavbarOpen(!isnavbarOpen)}
+            className="fixed z-50 right-4 top-4 w-8 h-8 flex flex-col justify-center items-center gap-1"
+          >
+            <motion.span
+              className="w-6 h-1 bg-white block"
+              animate={{
+                rotate: isnavbarOpen ? 45 : 0,
+                y: isnavbarOpen ? 4 : 0,
+              }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+            <motion.span
+              className="w-6 h-1 bg-white block"
+              animate={{
+                rotate: isnavbarOpen ? -45 : 0,
+                y: isnavbarOpen ? -4 : 0,
+              }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+          </button>
         </div>
-    )
+
+        <AnimatePresence>
+          {isnavbarOpen && (
+            <motion.div
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "100%" }}
+              transition={{ duration: 0.5 }}
+              className="fixed top-0 right-0 h-screen bg-gradient-to-r from-[#16151f] to-[#181622] md:w-[45vw] w-[90vw] z-40 shadow-2xl md:p-10 p-5 md:pt-0 pt-15"
+            >
+              <div className="flex rounded-full my-6 md:gap-3 justify-start mb-10">
+                {tabs.map((tab) => (
+                  <button
+                    key={tab}
+                    onClick={() => setSelectedtab(tab)}
+                    className={`md:px-5 px-3 py-3 md:text-md text-sm font-semibold rounded-full transition-all duration-300 ${
+                      selectedtab === tab
+                        ? "bg-white text-gray-900"
+                        : "text-gray-300 hover:text-white"
+                    }`}
+                  >
+                    {tab}
+                  </button>
+                ))}
+              </div>
+
+              <div className="text-white">
+                {selectedtab === "People" && (
+                  <div className="flex flex-col md:justify-center h-[60vh] pl-2">
+                    {peoples.map((e) => (
+                      <div
+                        key={e.name}
+                        className="hover:text-[#fff9cb] text-white md:text-6xl text-4xl mt-0.6 font-sans font-semibold md:tracking-tighter"
+                        onMouseEnter={() => handleImageChange(e.img)}
+                        onMouseLeave={() => handleImageReset()}
+                      >
+                        {e.name}
+                      </div>
+                    ))}
+                  </div>
+                )}
+
+                {selectedtab === "Places" && (
+                  <div className="flex flex-col md:justify-center h-[60vh] pl-2">
+                    {places.map((e) => (
+                      <div
+                        key={e.name}
+                        className="hover:text-[#fff9cb] text-white md:text-6xl text-4xl mt-0.6 font-sans font-semibold md:tracking-tight"
+                        onMouseEnter={() => handleImageChange(e.img)}
+                        onMouseLeave={() => handleImageReset()}
+                      >
+                        {e.name}
+                      </div>
+                    ))}
+                  </div>
+                )}
+
+                {selectedtab === "Trailers" && (
+                  <div>
+                    <p>Trailers content goes here</p>
+                  </div>
+                )}
+
+                {selectedtab === "Downloads" && (
+                  <div className="flex flex-col md:flex-row md:justify-center gap-3 flex-1 items-center justify-center">
+                    <div className="flex flex-col md:justify-center pl-2 gap-3 items-center justify-center overflow-y-scroll">
+                      {downloads.map((ele) => (
+                        <div
+                          className="flex flex-col bg-yellow-50 h-fit full relative"
+                          key={ele.name}
+                        >
+                          <motion.img
+                            src={ele.img}
+                            whileHover={{ scaleX: 0.99, scaleY: 0.97 }}
+                            transition={{ duration: 0.3 }}
+                            onHoverStart={() => setIsHovered(ele.name)}
+                            onHoverEnd={() => setIsHovered("")}
+                            className={`${
+                              isHovered === ele.name
+                                ? "saturate-100"
+                                : "saturate-65"
+                            } w-full h-full`}
+                          />
+                          <h1 className="absolute bottom-3 px-7 z-10 text-yellow-50 text-2xl font-sans font-bold tracking-tight">
+                            {ele.name}
+                          </h1>
+                          <motion.div
+                            className={`absolute bottom-3 right-0 mr-6 h-[40px] w-[40px] rounded-full ${
+                              isHovered === ele.name
+                                ? "bg-yellow-50"
+                                : "bg-[#37374f]"
+                            } z-10 flex justify-center items-center`}
+                          >
+                            <MoveRight
+                              className={`${
+                                isHovered === ele.name
+                                  ? "text-[#37374f]"
+                                  : "text-[#ffb0c4]"
+                              }`}
+                              size="18"
+                            />
+                          </motion.div>
+                        </div>
+                      ))}
+                      <div className="flex justify-center items-center px-4 bg-[#1d1d24] font-sans p-3 rounded-full font-semibold mt-3">
+                      See All Downloads
+                      <MoveRight size="20" className='ml-2'/>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+
+        <AnimatePresence mode="wait">
+          {isnavbarOpen && (
+            <motion.img
+              key={imageKey}
+              src={image}
+              className="w-[75vw] fixed left-0 z-10 top-0 object-cover h-full"
+              initial={{
+                x: 0,
+
+                opacity: 0,
+                filter: "blur(10px)",
+              }}
+              animate={{
+                x: "-10vw",
+
+                opacity: 1,
+                filter: "blur(0px)",
+              }}
+              exit={{
+                opacity: 0,
+                filter: "blur(5px)",
+                transition: { duration: 0.3 },
+              }}
+              transition={{
+                duration: 2,
+                ease: "easeOut",
+                scale: { duration: 0.6 },
+                filter: { duration: 0.4 },
+                opacity: { duration: 0.4 },
+              }}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+    );
 }
 
 export default Navbar
